6b22fc9c7244d2c029c3bb37374e220b01be1378
John Schulz <jschulz@twitter.com>
2012-12-14 18:45:49 -0800

    Define the prototype before creating instances with it.
    What do you know? Only test #2 (or #148) and tests are paying off.

--- test/test.js
+++ test/test.js
@@ -10,3 +10,13 @@ test('all Chromath.colors values are also available on Chromath', function (t) {
         t.ok(hasColor, 'has Chromath.' + color);
     });
 });
+
+test('all Chromath.colors are Chromath instances', function (t) {
+    var colors = Object.keys(Chromath.colors);
+
+    t.plan(colors.length);
+    for (var color in Chromath.colors) {
+        var isChromath = Chromath.colors[color] instanceof Chromath;
+        t.ok(isChromath, 'Chromath.colors.' + color + ' is Chromath');
+    }
+});
